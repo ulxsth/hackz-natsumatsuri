874d620f694bbcadf8e46c422e5276a6b7663dab
ulxsth <ulxsth@gmail.com>
2024-08-18 10:54:39 +0900

    feat: ping

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import { Client, GatewayIntentBits, IntentsBitField, REST, Routes } from 'discord.js';
+
+const TOKEN: string = process.env.DISCORD_TOKEN!;
+const CLIENT_ID: string = process.env.DISCORD_CLIENT_ID!;
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+const gatewayIntentValues = Object.values(GatewayIntentBits) as number[];
+const intents: number = gatewayIntentValues.reduce((a, b) => a | b);
+const client = new Client({ intents });
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user!.tag}`);
+});
+
+client.on("messageCreate", async (msg) => {
+  if (msg.content === "!ping") {
+    msg.reply("Pong!");
+  }
+});
+
+client.login(TOKEN);
